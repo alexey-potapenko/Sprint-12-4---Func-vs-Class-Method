de804ae983be7ac953fd2292d5a0990e28a46b23
alexey-potapenko <stgksa@gmail.com>
2025-03-26 01:40:48 +0300

    Add project files.

---
+++ Func vs Class Methos/ebook.cpp
@@ -0,0 +1,83 @@
+﻿#include <cstdint>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+class Book {
+private:
+    std::unordered_map<int, int> pages_for_users_; // max page for each user. user_id, max_page.
+    std::map<int, int> users_for_pages_; // number of users read page. page_number, number of users.
+
+    void UpdateUsersForPages(int user_id, int page_count) {
+        if (pages_for_users_.find(user_id) != pages_for_users_.end()) {
+            auto old_page = pages_for_users_[user_id];
+            users_for_pages_[old_page] -= 1;
+        }
+        if (users_for_pages_.find(page_count) == users_for_pages_.end()) {
+            users_for_pages_[page_count] = 1;
+        }
+        else {
+            users_for_pages_[page_count] += 1;
+        }
+    }
+
+public:
+    void Read(int user_id, int page_count) {
+        UpdateUsersForPages(user_id, page_count);
+        pages_for_users_[user_id] = page_count;
+    }
+
+    double Cheer(int user_id) {
+        if (pages_for_users_.find(user_id) == pages_for_users_.end()) {
+            return 0.;
+        }
+        else {
+            auto users_quantity = pages_for_users_.size(); // number of users
+            if (users_quantity == 1) {
+                return 1.;
+            }
+            else {
+                auto user_page = pages_for_users_[user_id];
+                auto max_users_for_pages_it = users_for_pages_.find(user_page);
+                if (max_users_for_pages_it == users_for_pages_.begin()) {
+                    return 0.;
+                }
+                int users_less_page = 0;
+                auto it = users_for_pages_.begin();
+                while (it != max_users_for_pages_it) {
+                    users_less_page += it->second;
+                    ++it;
+                }
+                return users_less_page / static_cast<double>(users_quantity - 1);
+            }
+        }
+    }
+};
+
+int main() {
+    Book book;
+    int32_t requests;
+    std::cin >> requests;
+    std::cin.ignore();
+    for (int32_t i = 0; i < requests; ++i) {
+        std::string request;
+        std::getline(std::cin, request);
+        auto space_pos = request.find_first_of(" ");
+        std::string request_type = request.substr(0, space_pos);
+        if (request_type == "CHEER") {
+            uint32_t user_id = std::stoi(request.substr(space_pos + 1));
+            auto result = book.Cheer(user_id);
+            std::cout << std::setprecision(6) << result << "\n";
+        } else if (request_type == "READ") {
+            auto second_space_pos = request.find_first_of(" ", space_pos + 1);
+            auto user_id = std::stoi(request.substr(space_pos, space_pos - second_space_pos));
+            auto page = std::stoi(request.substr(second_space_pos));
+            book.Read(user_id, page);
+        }
+    }
+}
